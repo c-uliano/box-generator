48d1adc726970c9488138e20a24720f09a2692a3
Colette Ulianogit config --global --listclear <culiano@live.com>
2023-02-21 19:20:49 -0500

    part 1 complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Form from './components/Form';
+import React, {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='w-50 m-auto pt-4'>
+            <h1>Box Generator</h1>
+            <Form />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Form.jsx
@@ -0,0 +1,47 @@
+import React, {useState} from 'react';
+import styles from './Form.module.css';
+
+const Form = (props) => {
+    // todo: this might be overdoing it, could be a simpler way to refactor this, but get it done first and work on that later
+    const [color, setColor] = useState('');
+    const [colorList, setColorList] = useState([]);
+
+    // event handlers
+    const onChangeHandler = (e) => {
+        setColor(e.target.value);
+    }
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        setColorList([...colorList, color]);
+        setColor("");
+    }
+
+    return (
+        <>
+            <form onSubmit={onSubmitHandler}>
+                <div className="mb-3">
+                    <label htmlFor="color" className="form-label"><b>Color</b></label>
+                    <input onChange={onChangeHandler} type="text" className="form-control" name="color" value={color} id="color" />
+                </div>
+                {/* todo: ninja challenge to add a second input that controls the width/height of box. Haven't done this challenge yet. */}
+                {/* <div className="mb-3">
+                    <label htmlFor="num" className="form-label"><b>Width/Height</b></label>
+                    <input onChange={onChangeHandler} type="number" className="form-control" name="num" value={num} id="num" />
+                </div> */}
+                <button type='submit' className='btn btn-primary mt-3'>Add</button>
+            </form>
+            {/* todo: maybe this gets broken out into a separate component */}
+            <h2>Color Blocks:</h2>
+            <div className='row'>
+            {
+                colorList.map((newColor, idx) => (
+                    <div key={idx} className={`col-md-auto ${styles.box}`} style={{backgroundColor: newColor}}></div>
+                ))
+            }
+            </div>
+        </>
+    );
+}
+
+export default Form
